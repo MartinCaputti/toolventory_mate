d97c2ef124cc58357a834b2e65ddd201e630494f
caputtimartin12gmail.com <caputtimartin12@gmail.com>
2024-12-02 16:36:06 -0300

    Proyecto Entregado
    -Se adjunta video Explicativo

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 //lib/main.dart
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart'; // Importa Firebase
 import 'package:flutter_native_splash/flutter_native_splash.dart'; // splash screen
